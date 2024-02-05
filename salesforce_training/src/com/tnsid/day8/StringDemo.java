136b2fbfe2ab966908b22ba50ff296d060c5bd1a
divya <divya@DESKTOP-1BV3GUL>
2024-02-05 17:56:25 +0530

    day8 commit

---
+++ salesforce_training/src/com/tnsid/day8/StringDemo.java
@@ -0,0 +1,12 @@
+package com.tnsid.day8;
+
+public class StringDemo {
+	public static void main(String[] args)
+	{
+		String s = new String("Divyanash");//way1
+		System.out.println(s);
+		String s2 = "Upadhyay";//way2
+		System.out.println(s2);
+	}
+
+}
