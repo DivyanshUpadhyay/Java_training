804e52d9a08fc0cc2d54b3d23114e7cf7e73b3fa
divya <divya@DESKTOP-1BV3GUL>
2024-02-06 18:19:38 +0530

    day9 commit

--- salesforce_training/src/com/tnsif/day9/StringApplications.java
+++ salesforce_training/src/com/tnsif/day9/StringApplications.java
@@ -10,12 +10,21 @@ public class StringApplications {
 		String s4 = new String("TNSIF");
 		//s1==s2 check whether s1 is equals to s2 or not
 		System.out.println("S1 == S2 :- "+(s1==s2));
+		//s1.equals(s2) check whether s1 is equals to s2 or not
+		System.out.println("S1 == S3 :- "+s1.equals(s2));
 		//s1==s3 check whether s1 is equals to s3 or not
 		System.out.println("S1 == S3 :- "+(s1==s3));
-		//s1==s3 check whether s4 is equals to s3 or not
+		//s1.equals(s3) check whether s1 is equals to s2 or not
+				System.out.println("S1 == S3 :- "+s1.equals(s3));
+		//s4==s3 check whether s4 is equals to s3 or not
 		System.out.println("S4 == S3 :- "+(s4==s3));
-		
-
+		//s4.equals(s3) check whether s4 is equals to s3 or not
+				System.out.println("S1 == S3 :- "+s4.equals(s3));
+				//print hash code
+				System.out.println(s1.hashCode());
+				System.out.println(s2.hashCode());
+				System.out.println(s3.hashCode());
+				System.out.println(s4.hashCode());
 	}
 
 }
