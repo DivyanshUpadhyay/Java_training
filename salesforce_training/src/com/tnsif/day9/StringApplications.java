3bc047baf22108e12d16b9cb6779c55ea2eb3db1
divya <divya@DESKTOP-1BV3GUL>
2024-02-06 18:05:00 +0530

    day9 initial commit

---
+++ salesforce_training/src/com/tnsif/day9/StringApplications.java
@@ -0,0 +1,21 @@
+package com.tnsif.day9;
+
+public class StringApplications {
+
+	public static void main(String args[])
+	{
+		String s1="TNSIF";
+		String s2= "TNSIF";
+		String s3 = new String("TNSIF");
+		String s4 = new String("TNSIF");
+		//s1==s2 check whether s1 is equals to s2 or not
+		System.out.println("S1 == S2 :- "+(s1==s2));
+		//s1==s3 check whether s1 is equals to s3 or not
+		System.out.println("S1 == S3 :- "+(s1==s3));
+		//s1==s3 check whether s4 is equals to s3 or not
+		System.out.println("S4 == S3 :- "+(s4==s3));
+		
+
+	}
+
+}
